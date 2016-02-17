92b5aba6ec4886a408ca3c51d71d88389626b715
Alexandru Cahnita <acahnita@ena.com>
2016-02-18 00:03:53 +0200

    updated desire page with dynamic profiles content

--- wp/oglinde/despre.php
+++ wp/oglinde/despre.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: content page
+Template Name: Despre
 */
 ?>
 
@@ -11,12 +11,30 @@ Template Name: content page
     <div style="position:absolute; width:100%; height:1px; z-index:5000000; border-bottom:1px solid #36ff00; top:10px;"></div>     
 <?php include("navigation-menu.php") ?>
 <div id="content" class="container">  
+ <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+<!-- <div class="row">
+	<div class="col-md-12">
+		<h1><?php the_title(); ?></h1>
+	</div>
+</div> -->
+<?php //include("portraits.php") ?>
+
+
+<!-- TEST CONTENT -->
+<?php
+$post_id = 93;
+$queried_post = get_post($post_id);
+?>
+<?php echo $queried_post->post_content; ?>
+
+<!-- TEST CONTENT -->
+
+
 <!-- START CONTENT -->
 <div class="row">
 <div class="col-md-12">
   <div class="col-md-6 col-md-offset-3">
- <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-                <h1><?php the_title(); ?></h1>
+
  <?php the_content(); ?>
 
 <?php endwhile; endif; ?>
--- wp/oglinde/portraits.php
+++ wp/oglinde/portraits.php
@@ -1,11 +1,33 @@
-
+<?php
+/*
+Template Name: Portraits
+*/
+?>
 <div class="row">
 <div class="col-md-12">
   <div class="col-md-12">
- <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
- <?php the_content(); ?>
-
-<?php endwhile; endif; ?>
+<div class="row">
+<div class="col-md-6">
+<img class="img img-responsive" src="http://dev1.gavrilescu.com/wp-content/uploads/2016/02/woman2.jpg">
+<h3>Name Surname</h3>
+<p>Here will be a small text, just a intro about the person, fi more details will be needed, we can always create a profile page for them</p>
+</div>
+<div class="col-md-6">
+<img class="img img-responsive" src="http://dev1.gavrilescu.com/wp-content/uploads/2016/02/woman1.jpg">
+<h3>Name Surname</h3>
+<p>Here will be a small text, just a intro about the person, fi more details will be needed, we can always create a profile page for them</p>
+</div>
+<div class="col-md-6">
+<img class="img img-responsive" src="http://dev1.gavrilescu.com/wp-content/uploads/2016/02/woman2.jpg">
+<h3>Name Surname</h3>
+<p>Here will be a small text, just a intro about the person, fi more details will be needed, we can always create a profile page for them</p>
+</div>
+<div class="col-md-6">
+<img class="img img-responsive" src="http://dev1.gavrilescu.com/wp-content/uploads/2016/02/woman1.jpg">
+<h3>Name Surname</h3>
+<p>Here will be a small text, just a intro about the person, fi more details will be needed, we can always create a profile page for them</p>
+</div>
+</div>
   </div><!-- col-md-6 -->
   </div><!--col-md-12-->
 </div><!-- .row -->  
